842d3e8d3d58e37e7e7495bdf6f5c5828ff3a7e1
Firdaus Alif Fahruddin <firdausalif.f21@gmail.com>
2018-08-30 13:13:38 +0700

    PWA init V1.0

---
+++ src/components/Login.vue
@@ -0,0 +1,119 @@
+<template>
+    <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-3" width="80">
+        <h1>Login to My-app</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <form>
+          <v-layout column>
+            <v-flex>
+              <v-text-field
+                name="login"
+                label="Email or Username"
+                id="login"
+                type="text"
+                required
+                single-line
+                box></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                :append-icon="passwordVisible ? 'visibility_off' : 'visibility'"
+                :type="passwordVisible ? 'text' : 'password'"
+                name="password"
+                label="Password"
+                single-line
+                box
+                @click:append="passwordVisible = !passwordVisible"
+              ></v-text-field>
+            </v-flex>
+            <v-btn
+              :loading="signInLoading"
+              :disabled="signInLoading"
+              color="info"
+              @click.native="loader = 'signInLoading'"
+              block
+            >
+              Sign In
+              <span slot="loader" class="custom-loader">
+                <v-icon light>cached</v-icon>
+              </span>
+            </v-btn>
+            <v-btn
+              color="success"
+              block
+            >
+              Don't have an account? Sign Up  
+            </v-btn>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  // import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        passwordVisible: false,
+        password: '',
+        signInLoading: false,
+        loader: null
+      }
+    },
+    watch: {
+      loader () {
+        const l = this.loader
+        this[l] = !this[l]
+
+        setTimeout(() => (this[l] = false), 3000)
+
+        this.loader = null
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>

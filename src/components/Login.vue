4503e3b5ee2066ec7d712b53fd47a767230e5753
Firdaus Alif Fahruddin <firdausalif.f21@gmail.com>
2018-08-31 09:19:49 +0700

    audit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -62,7 +62,7 @@
             large
             block
           >
-            Don't have an acoount? Register
+            Create an Account
           </v-btn>
         </v-flex>
     </v-container>
